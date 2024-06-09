5cd8fd7849776e02a996ac1217a36866298c3676
user <IsaevaAliya@github.com>
2024-06-09 23:22:06 +0600

    hw3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double[] numbers = {9.0, 6.0, 3.5, -2.5, 4.4, -2.6, -5.2, 11.4, -4.1, 1.8, -8.1, 9.8, -12.1, -6.4, 1.1};
+        boolean negative = false;
+        double sum = 0;
+        int count = 0;
+        for (double i : numbers
+        ) {
+            if (negative && i > 0) {
+                sum += i;
+                count++;
+            }
+            if (i < 0) {
+                negative=true;
+            }
+        }
+        double avg = sum / count;
+        System.out.println("среднее арифметическое: "+avg);
     }
 }
